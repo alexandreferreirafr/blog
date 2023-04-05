0e83abc8180a963f6c3d8aea272dad89aecff544
Alexandre Ferreira <alexandre.cicconi@yahoo.fr>
2023-04-05 18:56:02 +0000

    :add next og and image optimisations

---
+++ pages/api/og.tsx
@@ -0,0 +1,42 @@
+import { ImageResponse } from "@vercel/og";
+import { NextRequest } from "next/server";
+export const config = {
+  runtime: "edge",
+};
+export default function (request: NextRequest) {
+  const { searchParams } = request.nextUrl;
+  const title = searchParams.get('title') ?? 'Unveiling the secrets of Web Development'
+  const bgImage = searchParams.get('bgImage') ?? 'https://www.nakd-code.dev/assets/blog/dynamic-routing/cover.jpg'
+
+  const username = searchParams.get("username");
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          height: "100%",
+          width: "100%",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          backgroundImage: `url('${bgImage}')`,
+        }}
+      >
+        <div tw="flex bg-gray-50 bg-opacity-50">
+          <div tw="flex flex-col md:flex-row w-full py-12 px-4 md:items-center justify-between p-8">
+            <h2 tw="flex flex-col text-3xl sm:text-4xl font-bold tracking-tight text-gray-900 text-left">
+              <span tw="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">NAKD Code</span>
+              <span tw="text-indigo-600">
+                // {title}
+              </span>
+            </h2>
+          </div>
+        </div>
+      </div>
+    ),
+    {
+      width: 1200,
+      height: 600,
+    }
+  );
+}
--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -8,7 +8,6 @@ import Layout from '../../components/layout'
 import { getPostBySlug, getAllPosts } from '../../lib/api'
 import PostTitle from '../../components/post-title'
 import Head from 'next/head'
-import { CMS_NAME } from '../../lib/constants'
 import markdownToHtml from '../../lib/markdownToHtml'
 import type PostType from '../../interfaces/post'
 
@@ -20,7 +19,7 @@ type Props = {
 
 export default function Post({ post, morePosts, preview }: Props) {
   const router = useRouter()
-  const title = `${post.title} | Next.js Blog Example with ${CMS_NAME}`
+  const title = `${post.title} | NAKD Code`
   if (!router.isFallback && !post?.slug) {
     return <ErrorPage statusCode={404} />
   }
@@ -35,7 +34,7 @@ export default function Post({ post, morePosts, preview }: Props) {
             <article className="mb-32">
               <Head>
                 <title>{title}</title>
-                <meta property="og:image" content={post.ogImage.url} />
+                <meta property="og:image" content={`http://localhost:3000/api/og?title=${post.title}&bgImage=https://www.nakd-code.dev${post.ogImage.url}`} />
               </Head>
               <PostHeader
                 title={post.title}
